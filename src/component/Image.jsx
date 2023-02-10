1769c53943a8ff786962d55366f553836a968976
khushbu singh <114380485+k1k2k3k1@users.noreply.github.com>
2023-02-10 20:24:25 +0530

    asdf

---
+++ src/component/Image.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Details from "./Details";
+import "../App.css";
+
+export const Image = () => {
+  return (
+    <>
+      <div id="image">
+        <img src="https://i.pinimg.com/originals/58/91/2b/58912b2e3ad6a279347eebb47751a9c1.png" alt="" />
+      </div>
+      <Details />
+    </>
+  );
+};
